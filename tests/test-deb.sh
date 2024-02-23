e82b93b26ea0a9664a62e0685f9e2648baad46ff
Josh Beard <josh@joshbeard.me>
2024-02-22 23:43:54 -0700

    tests: add missing semi-colon

--- tests/test-deb.sh
+++ tests/test-deb.sh
@@ -6,12 +6,12 @@ REPO_URL="${REPO_BASE}/deb/"
 REPO_FILE="/etc/apt/sources.list.d/${PACKAGE_NAME}.list"
 
 docker run --rm debian:buster /bin/bash -c "
-    apt-get update && apt-get install -y ca-certificates wget gnupg2;
+    apt-get update && apt-get install -y ca-certificates wget gpg;
     wget -qO- https://${REPO_BASE}/gpg-pubkey.asc | gpg --dearmor > ${PACKAGE_NAME}-repo-keyring.gpg;
     mkdir -p /etc/apt/keyrings;
-    cat ${PACKAGE_NAME}-repo-keyring.gpg | tee /etc/apt/keyrings/${PACKAGE_NAME}-repo-keyring.gpg > /dev/null;
+    cat ${PACKAGE_NAME}-repo-keyring.gpg > /etc/apt/keyrings/${PACKAGE_NAME}-repo-keyring.gpg;
 
-    echo \"deb [arch=amd64 signed-by=/etc/apt/keyrings/${PACKAGE_NAME}-repo-keyring.gpg] ${REPO_URL} stable main\" |> tee /etc/apt/sources.list.d/${PACKAGE_NAME}.list;
+    echo \"deb [arch=amd64 signed-by=/etc/apt/keyrings/${PACKAGE_NAME}-repo-keyring.gpg] ${REPO_URL} stable main\" > /etc/apt/sources.list.d/${PACKAGE_NAME}.list;
     apt-get update && apt-get install -y $PACKAGE_NAME;
 
     if ! command -v $EXECUTABLE_NAME &> /dev/null; then
